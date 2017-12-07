b066c8a2b38dc9f5527879aefc0c6f479972843e
pauloXtr3m <paulo-gyn@outlook.com>
2017-12-07 13:48:45 -0200

    Implement edit and update command

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -7,13 +7,27 @@ class PortfoliosController < ApplicationController
     @portfolio_item = Portfolio.new
   end
 
-  # POST /blogs
-  # POST /blogs.json
   def create
     @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
     respond_to do |format|
       if @portfolio_item.save
         format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully created.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  def edit
+    @portfolio_item = Portfolio.find(params[:id])
+  end
+
+  def update
+    @portfolio_item = Portfolio.find(params[:id])
+
+    respond_to do |format|
+      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+        format.html { redirect_to portfolios_path, notice: 'The record successfully created.' }
       else
         format.html { render :new }
       end
