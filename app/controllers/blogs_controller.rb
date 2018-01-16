a63698cbad20f2cde403abf4894c76c4bd406a10
pauloXtr3m <paulo-gyn@outlook.com>
2018-01-15 22:45:49 -0200

    Training with debugger

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -56,6 +56,11 @@ class BlogsController < ApplicationController
     end
   end
 
+  def toggle_status
+    byebug
+    redirect_to_blogs_url
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_blog
